086c01d2c44e0fcad3b0e188365adf507e253a31
Imman-A-Josh <viratjosh4@gmail.com>
2025-10-01 17:28:15 +0530

    remove validation

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -5,6 +5,6 @@ const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/create", authMiddleware, paymentController.createRazorpayOrder);
 router.post("/verify", authMiddleware, paymentController.verifyPayment);
-router.post("/webhook", authMiddleware, paymentController.webhook);
+router.post("/webhook", paymentController.webhook);
 
 module.exports = router;
